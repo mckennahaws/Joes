0cab7baa872ade5ee50860f77f118a0d5ad03571
McKenna Spencer Account <10718674@my.uvu.edu>
2020-09-02 09:23:12 -0600

    upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.querySelector('#company').innerText="Joe's Bed and Breakfast";
+document.querySelector('#slogan').innerText="Cozy rooms, lucious food, stay a while.";
+
+let userName = prompt("What is your name?")
+let message = "Hello " + userName + ", welcome to the coziest lodge in Utah!"
+document.querySelector('#greeting').innerText= message;
